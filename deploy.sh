3b450f711f2bba090d56e665d84ec1b4ab34f1b0
danliu <danliu@wish.com>
2019-12-17 17:51:18 +0800

    Adjusted relative paths and escape '$'

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-touch .config_file_list
-cp ./git_func_config ./.config
-. ./.config
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+touch $DIR/.config_file_list
+cp $DIR/git_func_config $DIR/.config
+. $DIR/.config
 
 read -p "Input local config files: " input_config
 if [[ -n $input_config ]]
 then
-	echo "$input_config" >> .config_file_list
+	echo "$input_config" >> $DIR/.config_file_list
 fi
 
 read -p "Input critical branch names: " input_branch
 if [[ -n $input_branch ]]
 then
-	grep -v "critical_branches" ./.config > ./.config.temp
-	echo "export critical_branches=\"`echo $input_branch | tr ' ' '|' `\"" >> ./.config.temp
-	mv ./.config.temp ./.config
+	grep -v "critical_branches" $DIR/.config > $DIR/.config.temp
+	echo "export critical_branches=\"`echo $input_branch | tr ' ' '|' `\"" >> $DIR/.config.temp
+	mv $DIR/.config.temp $DIR/.config
 fi
 
 grep "git_func" $HOME/.bashrc > /dev/null 2>&1
@@ -26,7 +28,7 @@ then
 ## Add customized git functions
 source ~/.git_func/git_func
 function git_help() {
-	grep "function git_" ~/.git_func/git_func | awk -F " " '{print $2}' | awk -F "(" '{print $1}' | sort
+	grep "function git_" ~/.git_func/git_func | awk -F " " '{print \$2}' | awk -F "(" '{print \$1}' | sort
 }
 EOF
 fi
