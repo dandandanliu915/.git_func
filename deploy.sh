590cb4199ebb75251fbb3b10e9fcf73ad621a895
danliu <danliu@wish.com>
2020-01-04 12:28:27 +0800

    Added remote branch and cherry-pick function. Modified deployment steps.

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-touch $DIR/.config_file_list
-cp $DIR/git_func_config $DIR/.config
-. $DIR/.config
-
-read -p "Input local config files: " input_config
-if [[ -n $input_config ]]
-then
-	echo "$input_config" >> $DIR/.config_file_list
-fi
-
-read -p "Input critical branch names: " input_branch
-if [[ -n $input_branch ]]
-then
-	grep -v "critical_branches" $DIR/.config > $DIR/.config.temp
-	echo "export critical_branches=\"`echo $input_branch | tr ' ' '|' `\"" >> $DIR/.config.temp
-	mv $DIR/.config.temp $DIR/.config
-fi
-
 grep "git_func" $HOME/.bashrc > /dev/null 2>&1
 
 if [[ $? -ne 0 ]]
@@ -31,4 +11,33 @@ function git_help() {
 	grep "function git_" $DIR/git_func | awk -F " " '{print \$2}' | awk -F "(" '{print \$1}' | sort
 }
 EOF
+else
+        read -p "Already deployed. Redeploy? [Y/n] " yn
+        case $yn in
+    	    [Yy]* ) echo "Deploying ...";;
+            [Nn]* ) echo "Canceled"; exit;;
+            * ) echo "Canceled"; exit;;
+        esac
 fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+touch $DIR/.config_file_list
+cp $DIR/git_func_config $DIR/.config
+. $DIR/.config
+
+read -p "Input local config files (space separated): " input_config
+if [[ -n $input_config ]]
+then
+	echo "$input_config" > $DIR/.config_file_list
+fi
+
+read -p "Input critical branch names (space separated): " input_branch
+if [[ -n $input_branch ]]
+then
+	grep -v "CRITICAL_BRANCHES" $DIR/.config > $DIR/.config.temp
+	echo "export CRITICAL_BRANCHES=\"`echo $input_branch | tr ' ' '|' `\"" >> $DIR/.config.temp
+	mv $DIR/.config.temp $DIR/.config
+fi
+
+echo "Deployment finished"
